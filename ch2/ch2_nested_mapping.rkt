e073a23fd6662a92ad7874c99ec83b2ced844579
Anand1997 <anandnarawade.an@gmail.com>
2026-05-06 20:05:49 +0530

    - [CH02] solved EX 2.41.

--- ch2/ch2_nested_mapping.rkt
+++ ch2/ch2_nested_mapping.rkt
@@ -22,6 +22,7 @@ i = 3 → ((3 1) (3 2) (3 3))
 |#
 
 ;; NOTE: the result of (proc x) should work with append
+;; i.e it should return list to follow closure property
 (define (flatmap proc seq)
   (accum append nil (map proc seq)))
 
@@ -54,8 +55,54 @@ i = 3 → ((3 1) (3 2) (3 3))
 
 (define (subsets s)
   (if (null? s)
-      (list nil)
+      (list nil) ;;  empty set
       (flatmap (lambda (subset)
                  (list subset
                        (cons (car s) subset)))
-               (subsets (cdr s)))))
+               (subsets (cdr s))))) ;; subset of smaller problem
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j)
+                    (list i j))
+                  (enum-interval 1 (- i 1))))
+           (enum-interval 1 n)))
+
+#| Exercise 2.41
+  Write a procedure to find all ordered triples of distinct positive integers i, j, and k
+  less than or equal to a given integer n that sum to a given integer s.
+|#
+
+#|
+(define (triplate-sum-less-than-s n s)
+  (define (triplate-sum triplate)
+    (+ (car triplate)
+       (cadr triplate)
+       (caddr triplate)))
+  (define (create-triplates n)
+    (flatmap (lambda (i)
+               (flatmap (lambda (j)
+                          (map (lambda (k)
+                                (list i j k))
+                               (enum-interval (+ 1 j) n)))
+                        (enum-interval (+ 1 i) n)))
+             (enum-interval 1 n)))
+  (filter (lambda (x) (< x s)) (map triplate-sum (create-triplates n))))
+|#
+(define (triple-sum-equal-s n s)
+  (filter (lambda (triple)
+            (= (+ (car triple)
+                  (cadr triple)
+                  (caddr triple))
+               s))
+          (flatmap (lambda (i)
+                     (flatmap (lambda (j)
+                                (map (lambda (k)
+                                       (list i j k))
+                                     (enum-interval (+ j 1) n)))
+                              (enum-interval (+ i 1) n)))
+                   (enum-interval 1 n))))
+(newline)
+(display
+  (triple-sum-equal-s 6 10)
+  )
